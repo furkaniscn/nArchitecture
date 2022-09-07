affb64edc52a76ae6a78beccbd6d0748320e4274
furkaniscn <82982366+furkaniscn@users.noreply.github.com>
2022-09-07 23:00:24 +0300

    Add Model Feature

---
+++ src/demoProjects/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,32 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Model:Entity // Sen bir veritabanı nesnesisin.
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public virtual Brand? Brand { get; set; }
+
+        public Model()
+        {
+
+        }
+
+        public Model(int id, int brandId, string name, decimal dailyPrice, string ımageUrl):this()
+        {
+            Name = name;
+            DailyPrice = dailyPrice;
+            ImageUrl = ımageUrl;
+            BrandId = brandId;
+            Id = id;
+        }
+    }
+}
